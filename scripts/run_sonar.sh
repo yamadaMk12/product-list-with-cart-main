376c84862807305f71d6dd331dd59bcabcbdd184
Yamada MK <kite.kun555@gmail.com>
2025-11-13 17:28:36 +0100

    Update SonarQube analysis parameters

--- scripts/run_sonar.sh
+++ scripts/run_sonar.sh
@@ -3,7 +3,10 @@ set -e
 
 echo "=== Lancement de l'analyse SonarQube ==="
 sonar-scanner \
-  -Dsonar.projectKey=mon-projet \
+  -Dsonar.projectKey=ISEKAIMK \
+  -Dsonar.projectName=ISEKAI \
+  -Dsonar.projectVersion=1.0 \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=$SONAR_TOKEN
+  -Dsonar.token=$SONAR_TOKEN \
+  -Dsonar.sourceEncoding=UTF-8 \
