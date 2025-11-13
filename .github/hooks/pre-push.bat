58085327fc93a1c79606bd964638b3a1b3590434
ELHALIMI MOHAMED <MOHAMED.ELHALIMI@cmctta.ma>
2025-11-13 17:58:29 +0100

    test

---
+++ .github/hooks/pre-push.bat
@@ -0,0 +1,3 @@
+@echo off
+REM This calls Git Bash to run the actual pre-push script
+"C:\Program Files\Git\bin\bash.exe" "%~dp0pre-push.sh"
---
+++ .github/hooks/pre-push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "🚀 Pre-push hook triggered"
+# Put your SonarScanner command here
+# sonar-scanner ...
+exit 0
